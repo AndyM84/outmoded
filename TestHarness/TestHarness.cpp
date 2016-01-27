5c2deb65e1abc76c3e18f2639e319c6ec2ba02f2
Andrew Male <amale@zibings.com>
2016-01-26 21:30:35 -0500

    Now it's working in Windows, and pretty well I might add

--- TestHarness/TestHarness.cpp
+++ TestHarness/TestHarness.cpp
@@ -2,11 +2,27 @@
 
 #ifdef WIN32
 	#include <Windows.h>
+	#include <atlbase.h>
 #else
 	#include <term.h>
+	#include <unistd.h>
 #endif
 
+void write(LPCWSTR, short, short);
 void clear();
+void pauseApp(int);
+
+struct CellInfo
+{
+	LPCWSTR Value;
+	short X, Y;
+};
+
+CellInfo Box[] = {
+	{ L"X", 1, 1 }, { L"X", 2, 1 }, { L"X", 3, 1 },
+	{ L"X", 1, 2 }, { L"X", 2, 2 }, { L"X", 3, 2 },
+	{ L"X", 1, 3 }, { L"X", 2, 3 }, { L"X", 3, 3 }
+};
 
 int main()
 {
@@ -14,17 +30,42 @@ int main()
 	setupterm(NULL, fileno(stdout), NULL);
 #endif
 
-	std::cout << "Testing!" << std::endl;
+	int boxLen = sizeof(Box) / sizeof(CellInfo);
 
-	std::cin.get();
+	for (int i = 0; i < 300; ++i)
+	{
+		clear();
 
-	clear();
+		for (int j = 0; j < boxLen; ++j)
+		{
+			write(Box[j].Value, Box[j].X + i, Box[j].Y + i);
+		}
 
-	std::cout << "This is another test." << std::endl;
+		pauseApp(33);
+	}
 
 	std::cin.get();
 
 	return 0;
+}
+
+void write(LPCWSTR c, short x, short y)
+{
+#ifdef WIN32
+	COORD pos = { x, y };
+	DWORD dwBytesWritten = 0;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	if (hConsole == INVALID_HANDLE_VALUE)
+	{
+		return;
+	}
+
+	WriteConsoleOutputCharacter(hConsole, c, 1, pos, &dwBytesWritten);
+#else
+#endif
+
+	return;
 }
 
 // Credit where due: http://stackoverflow.com/a/6487534/563136
@@ -42,6 +83,22 @@ void clear()
 	SetConsoleCursorPosition(console, topLeft);
 #else
 	putp(tigetstr("clear"));
+#endif
+
+	return;
+}
+
+void pauseApp(int span)
+{
+	if (span < 0)
+	{
+		return;
+	}
+
+#ifdef WIN32
+	Sleep(span);
+#else
+	usleep(span * 1000);
 #endif
 
 	return;
