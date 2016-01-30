96b12c5f5783ba8265dd17dccecd2035996fbb43
Andrew Male <amale@zibings.com>
2016-01-29 21:14:36 -0500

    Back to where we were, but with shitty key comparison

--- TestHarness/TestHarness.cpp
+++ TestHarness/TestHarness.cpp
@@ -1,26 +1,134 @@
 #include <iostream>
 #include <Outmoded.h>
 
-//CellInfo Box[] = {
-//	{ 'X', 1, 1 }, { 'X', 2, 1 }, { 'X', 3, 1 },
-//	{ 'X', 1, 2 }, { 'X', 2, 2 }, { 'X', 3, 2 },
-//	{ 'X', 1, 3 }, { 'X', 2, 3 }, { 'X', 3, 3 }
-//};
-
 int main()
 {
+	bool keepRunning = true, needsRendered = true;
+	Outmoded::Point currentOffset(0, 0);
+	auto eng = Outmoded::Engine(true);
 	Outmoded::Point Box[] = {
 		{ 1, 1 }, { 2, 1 }, { 3, 1 },
 		{ 1, 2 }, { 2, 2 }, { 3, 2 },
 		{ 1, 3 }, { 2, 3 }, { 3, 3 }
 	};
 
-	for (int i = 0; i < 9; ++i)
+#ifdef _WIN32
+	eng.RegisterKey(VK_ESCAPE);
+	eng.RegisterKey(VK_RETURN);
+	eng.RegisterKey(VK_LEFT);
+	eng.RegisterKey(VK_RIGHT);
+	eng.RegisterKey(VK_UP);
+	eng.RegisterKey(VK_DOWN);
+#else
+	eng.RegisterKey(27);
+	eng.RegisterKey(10);
+	eng.RegisterKey(KEY_LEFT);
+	eng.RegisterKey(KEY_RIGHT);
+	eng.RegisterKey(KEY_UP);
+	eng.RegisterKey(KEY_DOWN);
+#endif
+
+	while (keepRunning)
 	{
-		std::cout << Box[i].ToString() << std::endl;
+		if (needsRendered)
+		{
+			int boxSize = sizeof(Box) / sizeof(Outmoded::Point);
+
+			eng.ClearScreen();
+
+			for (int i = 0; i < boxSize; ++i)
+			{
+				Outmoded::Point pos(Box[i].X + currentOffset.X, Box[i].Y + currentOffset.Y);
+				eng.SetCell(pos, 'X');
+			}
+
+			eng.Render();
+			needsRendered = false;
+		}
+
+		auto ch = eng.GetKeyInput();
+
+		if (ch.size() > 0)
+		{
+			for (auto key : ch)
+			{
+				switch (key)
+				{
+#ifdef _WIN32
+				case VK_RETURN:
+					currentOffset.X = 0;
+					currentOffset.Y = 0;
+					needsRendered = true;
+
+					break;
+				case VK_ESCAPE:
+					keepRunning = false;
+
+					break;
+				case VK_LEFT:
+					currentOffset.X -= 1;
+					needsRendered = true;
+
+					break;
+				case VK_RIGHT:
+					currentOffset.X += 1;
+					needsRendered = true;
+					
+					break;
+				case VK_UP:
+					currentOffset.Y -= 1;
+					needsRendered = true;
+
+					break;
+				case VK_DOWN:
+					currentOffset.Y += 1;
+					needsRendered = true;
+
+					break;
+#else
+				case 10:
+					currentOffset.X = 0;
+					currentOffset.Y = 0;
+					needsRendered = true;
+
+					break;
+				case 27:
+					keepRunning = false;
+
+					break;
+				case KEY_LEFT:
+					currentOffset.X -= 1;
+					needsRendered = true;
+
+					break;
+				case KEY_RIGHT:
+					currentOffset.X += 1;
+					needsRendered = true;
+
+					break;
+				case KEY_UP:
+					currentOffset.Y -= 1;
+					needsRendered = true;
+
+					break;
+				case KEY_DOWN:
+					currentOffset.Y += 1;
+					needsRendered = true;
+
+					break;
+#endif
+				default:
+					break;
+				}
+			}
+		}
+
+		eng.Pause(33);
 	}
 
-	std::cout << "Thanks for playing!" << std::endl;
+	eng.ClearScreen();
+
+	std::cout << std::endl << "Thanks for playing!" << std::endl;
 	std::cin.get();
 
 	return 0;
